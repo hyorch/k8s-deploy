253e708f8f7bd523a80d39075e9635fcd8859f05
hyorch <hyorch@gmail.com>
2021-09-20 17:59:25 +0200

    create namespace for metallb

--- 04-MetalLB/install.sh
+++ 04-MetalLB/install.sh
@@ -1,3 +1,4 @@
 helm repo add metallb https://metallb.github.io/metallb
 helm repo update
-helm install metallb metallb/metallb -f ./values.yaml
+kubectl create namespace metallb
+helm install -n metallb metallb metallb/metallb -f ./values.yaml
